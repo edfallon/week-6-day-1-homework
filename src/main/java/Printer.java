a03e9386b67d0d37c9c9ca293f6bd523494947ec
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-30 18:28:51 +0100

    Printer completed

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,28 @@
 public class Printer {
+    private int sheets;
+    private int toner;
+
+    public Printer(int sheets, int toner){
+        this.sheets = sheets;
+        this.toner = toner;
+    }
+
+    public int getSheets(){
+        return this.sheets;
+    }
+
+    public void print(int pages, int copies){
+        if ((this.sheets > pages * copies) & (this.toner > pages * copies)){
+            this.sheets -= (pages * copies);
+            this.toner -= (pages * copies);
+        }
+    }
+
+    public void refill(){
+        this.sheets = 100;
+    }
+
+    public int getToner(){
+        return this.toner;
+    }
 }
